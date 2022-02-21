fe6d6b086982e333550a8ccf7b2625ebf4a1a1fe
Tobias Maxham <tobias.maxham@tricept.de>
2022-02-21 15:26:01 +0100

    Upgrade to Laravel 9

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
-use App\Http\Controllers\DeployController;
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
-use TobyMaxham\Logger\LogViewerController;
 
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::get('/login/callback', [LoginController::class, 'callback']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::group(['middleware' => 'auth.simple'], function() {
-    Route::get('/', [DeployController::class, 'index'])->name('index');
-    Route::post('/', [DeployController::class, 'deploy']);
-    Route::get('log', [LogViewerController::class, 'index']);
-});
+//Route::get('/login', [LoginController::class, 'login'])->name('login');
+//Route::get('/login/callback', [LoginController::class, 'callback']);
+//
+//Route::group(['middleware' => 'auth.simple'], function() {
+//    Route::get('/', [DeployController::class, 'index'])->name('index');
+//    Route::post('/', [DeployController::class, 'deploy']);
+//    Route::get('log', [LogViewerController::class, 'index']);
+//});
